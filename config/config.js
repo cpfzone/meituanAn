212a8966ac53abf2da682b52b0c115cab703a594
孙航 <2662419405@qq.com>
2020-02-18 20:09:37 +0800

    :100: 上传头像

--- config/config.js
+++ config/config.js
@@ -8,54 +8,67 @@ export default {
     {
       path: '/',
       // 这里相对根目录,文件后缀名不能缺少
+      title: '美团项目',
       component: '../layouts/index.js',
       routes: [
         {
           path: '/login',
+          title: '手机美团登录',
           component: './login',
         },
         {
           path: '/account',
+          title: '账号-个人页面',
           component: './account',
         },
         {
           path: '/account/myinfo',
+          title: '修改用户信息',
           component: './account/myinfo',
         },
         {
           path: '/account/chous',
+          title: '订单',
           component: './account/chous',
         },
         {
           path: '/account/username',
+          title: '订单',
           component: './account/username',
         },
         {
           path: '/account/resetPass',
+          title: '订单',
           component: './account/resetPass',
         },
         {
           path: '/account/avatar',
+          title: '修改头像',
           component: './account/avatar',
         },
         {
           path: '/setPass',
+          title: '修改密码',
           component: './setPass',
         },
         {
           path: '/',
+          title: '首页',
           component: './index.js',
         },
         {
+          title: '搜索',
           path: '/search',
           component: './search/index',
         },
         {
+          title: '未找到相关信息',
           component: './NoFound',
         },
       ],
     },
     {
+      title: '未找到相关信息',
       component: './NoFound',
     },
   ],
