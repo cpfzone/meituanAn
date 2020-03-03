d9437af7a72363f575b0013067c73ec4ba3eebef
孙航 <2662419405@qq.com>
2020-03-03 08:28:21 +0800

    使用本地模式开发

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ module.exports = {
   port: 4001, // 后台启动的端口
   dbName: 'meituan', // 数据库名称
   secret: "it 's mySecret", //jwt秘钥
-  complete: true, // 如果设置为ture,那么后台和数据库都会调用我在服务器端的数据,如果你想要自己去修改后台和数据库,那么只要改为false即可
+  complete: false, // 如果设置为ture,那么后台和数据库都会调用我在服务器端的数据,如果你想要自己去修改后台和数据库,那么只要改为false即可
 };
